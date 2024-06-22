fbcd9e939f84d79a6e71ca008f300d3c6c5ef721
King To Li <ralap9@Kings-MacBook-Pro.local>
2024-06-22 12:57:00 +0800

    applying Card, ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -42,25 +42,18 @@ class MainApp extends StatelessWidget {
                   color: Colors.white,
                   margin:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Icon(Icons.phone, color: Colors.teal, size: 50.0),
-                        SizedBox(
-                          width: 20.0,
-                        ),
-                        Text(
-                          '+1 123 456 7890',
-                          style: TextStyle(
-                            fontFamily: 'Source Code Pro',
-                            fontSize: 20.0,
-                            color: Colors.teal,
-                            letterSpacing: 2,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(10.0),
+                    leading: Icon(Icons.phone, color: Colors.teal, size: 50.0),
+                    title: Text(
+                      '+1 123 456 7890',
+                      style: TextStyle(
+                        fontFamily: 'Source Code Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
@@ -68,25 +61,22 @@ class MainApp extends StatelessWidget {
                   color: Colors.white,
                   margin:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Icon(Icons.email, color: Colors.teal, size: 50.0),
-                        SizedBox(
-                          width: 20.0,
-                        ),
-                        Text(
-                          '123@123.com',
-                          style: TextStyle(
-                            fontFamily: 'Source Code Pro',
-                            fontSize: 20.0,
-                            color: Colors.teal,
-                            letterSpacing: 2,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(10.0),
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 50.0,
+                    ),
+                    title: Text(
+                      "I7iZK@example.com",
+                      style: TextStyle(
+                        fontFamily: 'Source Code Pro',
+                        fontSize: 17.0,
+                        color: Colors.teal,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),

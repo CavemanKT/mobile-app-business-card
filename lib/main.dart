235726214b306bb4b9c5352c4dca39678cf4d494
King To Li <ralap9@Kings-MacBook-Pro.local>
2024-06-22 12:48:54 +0800

    v1: Card Padding business card

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,98 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Li, King To',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Application Developer',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'likingto@hotmail.com',
-                style: TextStyle(fontSize: 14),
-              ),
-            ],
-          ),
-        ),
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/acorn_kt.png'),
+                ),
+                Text(
+                  'acorn_kt',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Code Pro',
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.phone, color: Colors.teal, size: 50.0),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Text(
+                          '+1 123 456 7890',
+                          style: TextStyle(
+                            fontFamily: 'Source Code Pro',
+                            fontSize: 20.0,
+                            color: Colors.teal,
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.email, color: Colors.teal, size: 50.0),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Text(
+                          '123@123.com',
+                          style: TextStyle(
+                            fontFamily: 'Source Code Pro',
+                            fontSize: 20.0,
+                            color: Colors.teal,
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
